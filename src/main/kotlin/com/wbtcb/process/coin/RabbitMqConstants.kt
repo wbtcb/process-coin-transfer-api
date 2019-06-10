7824c65b7ae1186aacaeb0e6c6f9ec68311821d4
Lubos Lacina <lacom@email.cz>
2019-06-10 11:03:52 +0200

    Add state Rejected

--- src/main/kotlin/com/wbtcb/process/coin/RabbitMqConstants.kt
+++ src/main/kotlin/com/wbtcb/process/coin/RabbitMqConstants.kt
@@ -18,5 +18,17 @@ class RabbitMqConstants {
         const val PAYMENT_EXCHANGE = "payment"
 
         const val PAYMENT_COIN_ROUTING_PREFIX_KEY = "$PAYMENT_EXCHANGE.coin"
+
+        /**
+         * Inbound (and Outbound) process
+         */
+        const val COIN_WALLET_EXCHANGE = "coin-wallet"
+
+        const val COIN_WALLET_INBOUND_ROUTING_PREFIX_KEY = "$COIN_WALLET_EXCHANGE.inbound"
+
+        /**
+         * TODO this is not needs so far is not implemented
+         */
+        const val COIN_WALLET_OUTBOUND_ROUTING_PREFIX_KEY = "$COIN_WALLET_EXCHANGE.outbound"
     }
 }
