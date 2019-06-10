19cd18bf021c365ca464d2a7d4b419158ef50cfd
Lubos Lacina <lacom@email.cz>
2019-06-10 10:56:48 +0200

    Add state Rejected

--- src/main/kotlin/com/wbtcb/process/coin/dto/message/InboundTransferUpdateMessage.kt
+++ src/main/kotlin/com/wbtcb/process/coin/dto/message/InboundTransferUpdateMessage.kt
@@ -7,7 +7,7 @@ import java.math.BigDecimal
 import java.util.UUID
 
 data class InboundTransferUpdateMessage constructor(
-    val externalId: UUID,
+    val orderRef: UUID,
     val channelCode: String,
     val currencyCode: String,
     val addressTo: String?,
