393c1f83bb709b873bac0c71db2dcbc3437cf127
Lubos Lacina <lacom@email.cz>
2019-06-10 09:58:21 +0200

    Add state Rejected

--- src/main/kotlin/com/wbtcb/process/coin/enum/PaymentStatus.kt
+++ src/main/kotlin/com/wbtcb/process/coin/enum/PaymentStatus.kt
@@ -1,10 +1,32 @@
 package com.wbtcb.process.coin.enum
 
 enum class PaymentStatus {
+    /**
+     * Incoming request to PAY
+     */
     CREATED,
+    /**
+     * Valid message
+     */
     VALID,
+    /**
+     * Invalid message
+     */
     INVALID,
+    /**
+     * Prepare to pay
+     */
     TO_PAY,
+    /**
+     * Cannot pay rejected by wallet
+     */
+    REJECTED,
+    /**
+     * In process paying (waiting to confirm transaction)
+     */
     PENDING,
+    /**
+     * Paid
+     */
     PAID
 }
